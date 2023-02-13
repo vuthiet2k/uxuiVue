66d9ff974c833d7b2ce2d7855d2d29c152b29a9a
vuthiet2k <vuthiet2k@gmail.com>
2023-02-13 15:27:37 +0700

    fix bug add task

--- firstdjango/thiethv/static/thiethv/js/component/data_tool/add_task.js
+++ firstdjango/thiethv/static/thiethv/js/component/data_tool/add_task.js
@@ -13,7 +13,7 @@ const addTaskIcon = {
     <div class="ms-3 position-relative"
          style="background-color:#fff; border-radius: 50%; width: 50px; height: 50px;">
         <h2 title="Thêm mới một công việc" class="cursor-pointer" style="size: 32px; text-align: center;line-height: 50px;" @click="handleClickView">+</h2>
-        <div class="position-absolute" style="background-color: #fff; width: 250px; z-index: 1;
+        <div class="position-absolute" style="background-color: #fff; width: 250px; z-index: 3;
          box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);top: 55px;" :style="setPropNew" :class="{'d-none': !view}">
             <div class="pt-2 pe-3" style="text-align: right;"><span class="cursor-pointer" @click="handleClickView">Đóng</span></div>
             <div class="m-3">
@@ -22,6 +22,7 @@ const addTaskIcon = {
                 <div class="cursor-pointer mb-3 p-2" style="border: 1px dotted #ccc; border-radius: 5px;" @click="handleClickForm(3)">Form 3</div>
             </div>
         </div>
+        <div :class="{'d-none': !view}" style="position: fixed; inset: 0; z-index: 2" @mousedown="handleClickView"></div>
     </div>`,
     data: function () {
         return {
--- firstdjango/thiethv/static/thiethv/js/component/data_tool/manage_task.js
+++ firstdjango/thiethv/static/thiethv/js/component/data_tool/manage_task.js
@@ -21,26 +21,15 @@ const manageTask = {
         <div class="d-flex ms-3 mb-3">
             <add-task-icon :newIcon="true" @chose-form="emitTypeForm"></add-task-icon>
         </div>  
-        <div v-for="item, index in totalTask" class="d-flex ms-3 mb-3" :key="index">
-            <create-task :typeForm="item"></create-task>
+        <div v-for="item, index in totalTask" class="d-flex ms-3 mb-3" :key="item.key">
+            <create-task :typeForm="item.formType"></create-task>
             <add-task-icon :index="index+1" @chose-form="emitTypeForm"></add-task-icon>
         </div>
     </div>`,
     methods: {
         emitTypeForm: function (formId, indexId) {
             const vm = this;
-            vm.totalTask.splice(indexId, 0, formId);
-            // const length = vm.totalTask.length;
-            // if (indexId === length) {
-            //     vm.totalTask.push(formId);
-            //     return;
-            // }
-            // let arrTemp = [...vm.totalTask, 1];
-            // arrTemp[indexId] = formId;
-            // for (let i = indexId; i < length; i++) {
-            //     arrTemp[i + 1] = vm.totalTask[i];
-            // }
-            // vm.totalTask = [...arrTemp];
+            vm.totalTask.splice(indexId, 0, {formType: formId, key: (new Date()).getTime()});
         },
     }
 }
